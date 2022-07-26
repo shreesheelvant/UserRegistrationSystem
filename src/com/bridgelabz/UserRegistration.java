35bc0049565f9fe753c99fc2d62e25f138c43fe8
unknown <suntanabhagyashree@gmail.com>
2022-07-26 21:50:56 +0530

    ADDED:Enter a valid first name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,10 +1,20 @@
 package com.bridgelabz;
 
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
             System.out.println("Welcome to user registration program");
-    }
-}
+               Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter first name");
+                String firstName = scanner.next();
+                System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+
+            }
+
+        }
+
+
 
