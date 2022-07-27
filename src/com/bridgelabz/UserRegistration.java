74f9b76b5035d0a9e617817f29bb6bb27f3a76a5
unknown <suntanabhagyashree@gmail.com>
2022-07-27 11:45:02 +0530

    ADDED:Enter a Valid Email

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -16,12 +16,15 @@ public class UserRegistration {
                 String lastName = scanner.next();
                 System.out.println(Pattern.matches("[A-Z][a-z]{2,}",lastName));
 
-            }
-
-        }
-
-
-
-
+                System.out.println("Enter emailId");
+                String emailId = scanner.next();
+                System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,}", emailId));
+    }
+}
 
 
+
+                              
+
+
+                                               
